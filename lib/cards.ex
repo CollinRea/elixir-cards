8a3189766d18f7c18db76cb39c8cc9b939ca4057
Collin Rea <collinr01@gmail.com>
2017-02-04 12:55:38 -0600

    Add shuffle and contains? method to Cards module using Elixer standard library Enum methods

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,5 +1,13 @@
 defmodule Cards do
   def create_deck do
-    ["Ace", "Two", "Three"]
+    ["Ace", "Two", "Three", "Four"]
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
