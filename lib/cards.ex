eb508bddcbe050ef484c6d9b98c2530052e65ea1
Collin Rea <collinr01@gmail.com>
2017-02-05 07:03:42 -0600

    Add ExDoc to project dependencies, add comments to cards.ex to be translated by ExDoc.

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+  """
+
+  @doc """
+    Returns a list of strings representing a deck of playing cards.
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -8,28 +15,54 @@ defmodule Cards do
     end
   end
 
+  @doc """
+    Takes a deck and returns a shuffled deck.
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Takes a deck and a card and returns a boolean if the card can be found in the deck.
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indiciates how many cards should be in a hand.
+    Returns a tuple.
+  """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
+  @doc """
+    Takes a `deck` and `filename`, saves the deck as `filename` to local storage.
+  """
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
     File.write(filename, binary)
   end
 
+  @doc """
+    Takes a `filename` and returns the saved deck.
+  """
   def load(filename) do
-    {status, binary} = File.read(filename)
-    case status do
-      :ok -> :erlang.binary_to_term(binary)
-      :error -> "That file does not exist"
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, reason} -> "That file does not exist."
     end
+  end
+
+  @doc """
+    Creates a deck, shuffles cards in deck and returns tuple of `hand_size`
+    and deck of remaining cards like the deal method.
+  """
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> shuffle
+    |> deal(hand_size)
   end
 end
